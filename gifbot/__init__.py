6079b1f7437159471d8dc6f168eb091a62d751bd
Abhijith NT <abhijithnt64@gmail.com>
2021-01-27 13:22:17 +0530

    update

--- gifbot/__init__.py
+++ gifbot/__init__.py
@@ -1,48 +1,45 @@
 """
 Copyright 2021 ABHIJITH N T
 """
-import logging
-import os
-
+import logging, os
 from telegram import *
 from telegram.ext import *
-
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO)
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
 
-class BotEnv:
-    ENV = bool(os.environ.get("ENV", False))
+class BotEnv():
+    ENV = bool(os.environ.get('ENV', False))
     try:
         if ENV:
-            TOKEN = os.environ.get("BOT_TOKEN")
+            TOKEN = os.environ.get('BOT_TOKEN')
         else:
             from sample_config import config
-
             TOKEN = config.BOT_TOKEN
     except KeyError:
-        logger.error(
-            "One or more configuration values are missing exiting now.")
+        logger.error('One or more configuration values are missing exiting now.')
         exit(1)
-
-
 class gifBot:
     Bot = Bot(BotEnv.TOKEN)
-
 
 class Msg:
     START = " 😅\nYou can use me in InLine mode and search for available GIFS.\n"
     WL_GIF = "https://media.tenor.com/images/baebd65d376f80120d6d008139f76a16/tenor.gif"
     keyboard = [
         [
-            InlineKeyboardButton(text="Search Inline",
-                                 switch_inline_query_current_chat="")
-        ],
-        [
             InlineKeyboardButton(
-                text="📚 Source",
-                url="https://github.com/Abhijith-cloud/TGInlineGIF/")
-        ],
-    ]
+                text = "Search Inline",
+                switch_inline_query_current_chat = ""
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                text = "📚 Source",
+                url = "https://github.com/Abhijith-cloud/TGInlineGIF/"
+                )
+            ]
+        ]
+
+
