15f74982e39fa9c1b2e73f4e70a82091fbb1350e
AbhijithNT <abhijithnt64@gmail.com>
2021-12-30 23:34:41 +0530

    arrange

--- gifbot/__main__.py
+++ gifbot/__main__.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python
-import os
-from telegram import *
 from telegram.ext import *
-from .cmd import startMsg
-from .gif import  gif_inlinequery
-from gifbot import BotEnv, logger
+from .cmd import start_message
+from .gif import gif_inline_query
+from gifbot import logger, BOT_TOKE, PORT, WEBHOOK_URL
+
+init_logger = logger(__name__)
 
 
 def main() -> None:
-    updater = Updater(BotEnv.TOKEN, use_context=True)
+    updater = Updater(BOT_TOKE, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler(["start", "help", "h"], startMsg))
     dispatcher.add_handler(InlineQueryHandler(gif_inlinequery))
@@ -23,6 +22,7 @@ def main() -> None:
     else:
         updater.start_polling()
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
