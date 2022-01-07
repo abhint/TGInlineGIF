57ba172eea4f55d82e47571a9a1a15e07d911a62
Abhijith N T <abhijithnt64@gmail.com>
2022-01-07 19:56:54 +0530

    Revert "webhook removed (#14)"

--- gifbot/__main__.py
+++ gifbot/__main__.py
@@ -1,7 +1,7 @@
 from telegram.ext import *
 from .cmd import start_message
 from .gif import gif_inline_query
-from gifbot import logger, BOT_TOKE
+from gifbot import logger, BOT_TOKE, PORT, WEBHOOK_URL
 
 init_logger = logger(__name__)
 
@@ -11,8 +11,12 @@ def main() -> None:
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler(["start", "help", "h"], start_message))
     dispatcher.add_handler(InlineQueryHandler(gif_inline_query))
-    init_logger.info('Bot is Online!')
-    updater.start_polling()
+    updater.start_webhook(
+        webhook_url=WEBHOOK_URL,
+        port=PORT
+    )
+    init_logger.info('Bot is start')
+    # updater.start_polling()
     updater.idle()
 
 
