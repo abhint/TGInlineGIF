5f9fb054c0239da7e866c9f1785233731757956f
abhint <notifyabhijith@gmail.com>
2023-01-03 11:05:52 +0530

    python-telegram-bot 20.0

--- gifbot/__main__.py
+++ gifbot/__main__.py
@@ -1,23 +1,26 @@
 from telegram.ext import *
-from .cmd import start_message
+from .cmd import start_command
 from .gif import gif_inline_query
-from gifbot import logger, BOT_TOKE, PORT, WEBHOOK_URL
+from gifbot import logger, BOT_TOKE, WEBHOOK_URL, PORT
 
 init_logger = logger(__name__)
 
 
 def main() -> None:
-    updater = Updater(BOT_TOKE, use_context=True)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler(["start", "help", "h"], start_message))
-    dispatcher.add_handler(InlineQueryHandler(gif_inline_query))
-    updater.start_webhook(
+    application = Application.builder().token(BOT_TOKE).build()
+    application.add_handler(CommandHandler(["start", "help", "h"], start_command))
+    application.add_handler(InlineQueryHandler(gif_inline_query))
+    application.run_webhook(
+        port=PORT,
         webhook_url=WEBHOOK_URL,
-        port=PORT
     )
+    # application.run_webhook(
+    #     webhook_url=WEBHOOK_URL,
+    #     port=PORT,
+    #     # url_path=BOT_TOKE,
+    #     # webhook_url=f'{WEBHOOK_URL}/{BOT_TOKE}',
+    # )
     init_logger.info('Bot is start')
-    # updater.start_polling()
-    updater.idle()
 
 
 if __name__ == '__main__':
